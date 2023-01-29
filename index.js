37070d58ef90b8527ef7285f9ac8cdd820630d52
Lebzadid <114964064+Lebzadid@users.noreply.github.com>
2023-01-28 18:15:59 -0700

    commit

---
+++ index.js
@@ -0,0 +1,30 @@
+<script>
+function myFunction1() {
+   let userInput = document.querySelector("#userInput");
+   let message = document.querySelector("#message");
+
+   message1.innerHTML = "Welcome to my SimpleJs webpage, " + userInput.value;
+
+}
+</script>
+
+<script>
+function myFunction2() {
+let userHeight = document.querySelector("#userHeight");
+let message = document.querySelector("#message");
+
+message2.innerHTML = "Great. You are, " + userHeight.value;
+
+}
+</script>
+
+<script>
+function myFunction3() {
+let userCountry = document.querySelector("#userCountry");
+let message = document.querySelector("#message");
+
+message3.innerHTML = "Awesome. You are from, " + userCountry.value;
+
+}
+</script>
+
